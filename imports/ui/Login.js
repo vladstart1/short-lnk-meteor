2b55720103721828220f80a7a667606212c578c6
Vlad Belcar <vladstart1@hotmail.com>
2017-09-17 15:00:16 +0300

    Add readme docs

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -29,7 +29,7 @@ class Login extends React.Component {
     return (
       <div className="boxed-view">
         <div className="boxed-view__box">
-          <h1>Short Link</h1>
+          <h1>Short Link Login</h1>
 
             {this.state.error ? <p>{this.state.error}</p> : undefined}
 
