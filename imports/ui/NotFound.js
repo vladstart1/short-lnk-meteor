da4b44bcd470cddcfd9f8725f6daeb87c4d9a48b
Vlad Belcar <vladstart1@hotmail.com>
2017-09-17 15:32:33 +0300

    404

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -6,7 +6,7 @@ export default () => {
     <div className="boxed-view">
       <div className='boxed-view__box'>
         <h1>Page Not Found</h1>
-        <p>You sure that u want to see 401?</p>
+        <p>You sure that u want to see 404?</p>
         <Link to="/" className="button button--link">Head home</Link>
       </div>
     </div>
